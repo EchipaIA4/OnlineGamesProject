b0ec0cdc780284b6653adee3485d5385b8cbaa01
Mihai Dumitrescu <mihairazvand1@gmail.com>
2025-12-07 18:50:05 +0200

    Make the item name disappear after 2.5 seconds after hover
    Signed-off-by: Mihai Dumitrescu <mihairazvand1@gmail.com>

--- game_source/ghost_in_the_files/entities/file.py
+++ game_source/ghost_in_the_files/entities/file.py
@@ -86,6 +86,7 @@ class FileIcon:
             if inventory:
                 inventory.remove_item(item)
             self.sprite = pygame.image.load("assets/sprites/file.png").convert_alpha()
+            self.sprite = pygame.transform.scale(self.sprite, (63, 63))
             self.rect = self.sprite.get_rect(center=self.block.center)
        
     def render(self, surface):
